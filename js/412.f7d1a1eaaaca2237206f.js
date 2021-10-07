ecf9b58db583e29176a8272afd8ff864a533c5c0
Travis CI User <travis@example.org>
2021-10-07 09:26:04 +0000

    Merge pull request #518 from karelhala/improved-proxy-config
    Add env variables to run webpack proxy much smoother

--- js/412.f7d1a1eaaaca2237206f.js
+++ js/412.f7d1a1eaaaca2237206f.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksettings_frontend=self.webpackChunksettings_frontend||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>p});var r=n(92950),o=n.n(r),s=n(45697),a=n.n(s),l=n(334),c=n(45237),u=n(30319),f=n(87999),g=n(35240),i=function(e){var t=e.logger;return o().createElement(c.Provider,{store:(0,u.S1)(t).getStore()},o().createElement(l.BrowserRouter,{basename:(0,g.eb)(window.location.pathname,1)},o().createElement(f.Z,null)))};i.propTypes={logger:a().func};const p=i}}]);
-//# sourceMappingURL=../sourcemaps/412.980089cf24648cae36df9df91a0d6a2f.js.map
+//# sourceMappingURL=../sourcemaps/412.b61b749028825f5001a22412af89e367.js.map
